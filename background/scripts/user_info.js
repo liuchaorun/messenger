1c0e1346b68f4b8c96a089587148c4a89c418390
Soulike <316413310@qq.com>
2017-06-27 17:41:21 +0800

    change UI

--- background/scripts/user_info.js
+++ background/scripts/user_info.js
@@ -1,3 +1,15 @@
 /**
  * Created by 31641 on 2017-6-25.
  */
+/**
+ <p><span class="glyphicon glyphicon-user"></span> 用户名：<span id="username"></span></p>
+ <p><span class="glyphicon glyphicon-envelope"></span> 邮箱：<span id="email"></span></p>
+ <p><span class="glyphicon glyphicon-phone"></span> 屏幕数：<span id="screen_num"></span></p>
+ <p><span class="glyphicon glyphicon-picture"></span> 图片数：<span id="picture_num"></span></p>
+ <p><span class="glyphicon glyphicon-log-in"></span> 上次登录时间：<span id="last_login_time"></span></p>
+ **/
+
+$(function ()
+{
+
+});
