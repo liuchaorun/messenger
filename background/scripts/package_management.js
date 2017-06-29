54346429df823fbebef17ac9220df6ebc8043c74
Soulike <316413310@qq.com>
2017-06-29 20:39:06 +0800

    fix bugs

--- background/scripts/package_management.js
+++ background/scripts/package_management.js
@@ -48,6 +48,8 @@ $(function ()
  *      {id,url},
  *      {id,url}
  * ]
+ *
+ * <input type="text" class="form-control" id=${}.id_time class='picture_time_input'>
  * **/
 function image_AJAX(table_id, button_id, footer_id)
 {
@@ -75,23 +77,23 @@ function image_AJAX(table_id, button_id, footer_id)
                     {
                         $(`#${table_id}`).append(` <div class="add_modal_row">
  <div class="add_modal_cell">
- <label id=${pictures[row * 5].id}><img src=${pictures[row * 5].src} alt=${pictures[row * 5].id} class="image img-responsive"><input type="checkbox"        class="add_checkbox"></label>
+ <label id=${pictures[row * 5].id}><img src=${pictures[row * 5].src} alt=${pictures[row * 5].id} class="image img-responsive"><input type="checkbox"        class="add_checkbox"><input type="text" class="form-control" id=${pictures[row * 5].id}_time class='picture_time_input'></label>
  </div>
  </div>
  <div class="add_modal_cell">
- <label id=${pictures[row * 5 + 1].id}><img src=${pictures[row * 5 + 1].src} alt=${pictures[row * 5 + 1].id} class="image img-responsive"><input type="checkbox"        class="add_checkbox"></label>
+ <label id=${pictures[row * 5 + 1].id}><img src=${pictures[row * 5 + 1].src} alt=${pictures[row * 5 + 1].id} class="image img-responsive"><input type="checkbox"        class="add_checkbox"><input type="text" class="form-control" id=${pictures[row * 5 + 1].id}_time class='picture_time_input'></label>
  </div>
  </div>
  <div class="add_modal_cell">
- <label id=${pictures[row * 5 + 2].id}><img src=${pictures[row * 5 + 2].src} alt=${pictures[row * 5 + 2].id} class="image img-responsive"><input type="checkbox"        class="add_checkbox"></label>
+ <label id=${pictures[row * 5 + 2].id}><img src=${pictures[row * 5 + 2].src} alt=${pictures[row * 5 + 2].id} class="image img-responsive"><input type="checkbox"        class="add_checkbox"><input type="text" class="form-control" id=${pictures[row * 5 + 2].id}_time class='picture_time_input'></label>
  </div>
  </div>
  <div class="add_modal_cell">
- <label id=${pictures[row * 5 + 3].id}><img src=${pictures[row * 5 + 3].src} alt=${pictures[row * 5 + 3].id} class="image img-responsive"><input type="checkbox"        class="add_checkbox"></label>
+ <label id=${pictures[row * 5 + 3].id}><img src=${pictures[row * 5 + 3].src} alt=${pictures[row * 5 + 3].id} class="image img-responsive"><input type="checkbox"        class="add_checkbox"><input type="text" class="form-control" id=${pictures[row * 5 + 3].id}_time class='picture_time_input'></label>
  </div>
  </div>
  <div class="add_modal_cell">
- <label id=${pictures[row * 5 + 4].id}><img src=${pictures[row * 5 + 4].src} alt=${pictures[row * 5 + 4].id} class="image img-responsive"><input type="checkbox"        class="add_checkbox"></label>
+ <label id=${pictures[row * 5 + 4].id}><img src=${pictures[row * 5 + 4].src} alt=${pictures[row * 5 + 4].id} class="image img-responsive"><input type="checkbox"        class="add_checkbox"><input type="text" class="form-control" id=${pictures[row * 5 + 4].id}_time class='picture_time_input'></label>
  </div>
  </div>
  </div>`)
@@ -102,7 +104,7 @@ function image_AJAX(table_id, button_id, footer_id)
                         $(`#${table_id}`).append(`<div class="add_modal_row" id="last_row"></div>`);
                         for (let i = 0; i < pictures.length - row * 5; i++)
                         {
-                            $('#last_row').append(`<div class="add_modal_cell"><label id=${pictures[row * 5 + i].id}><img src=${pictures[row * 5 + i].src} alt=${pictures[row * 5 + i].id} class="image img-responsive"><input type="checkbox" class="add_checkbox"></label></div></div>`)
+                            $('#last_row').append(`<div class="add_modal_cell"><label id=${pictures[row * 5 + i].id}><img src=${pictures[row * 5 + i].src} alt=${pictures[row * 5 + i].id} class="image img-responsive"><input type="checkbox" class="add_checkbox"><input type="text" class="form-control" id=${pictures[row * 5 + i].id}_time class='picture_time_input'></label></div></div>`)
                         }
                     }
                     activate_checkbox();
