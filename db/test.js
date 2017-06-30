df43e0c35d655f57ba8193e0bfedee84e82be3e8
lcr <1558531230@qq.com>
2017-06-30 20:26:44 +0800

    1.0

--- db/test.js
+++ db/test.js
@@ -1,38 +1,41 @@
 /**
  * Created by lcr on 17-5-16.
  */
-// const model = require('./model');
-// const config = require('./config');
-// const Sequelize = require('sequelize');
-// let sequelize = new Sequelize(config.database, config.username, config.password, {
-//     host: config.host,
-//     dialect: 'postgres',
-//     timezone:'+08:00',
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         idle: 3000
-//     }
-// });
-// let user = model.user;
-// let screen = model.screen;
-// let picture = model.picture;
-// let resource = model.resource;
-// let resource_picture = model.resource_picture;
-// user.hasMany(picture,{foreignKey:'user_id'});
-// user.hasMany(screen,{foreignKey:'user_id'});
-// user.hasMany(resource,{foreignKey:'user_id'});
-// screen.hasMany(resource,{foreignKey:'screen_id'});
-// resource.belongsToMany(picture,{through:resource_picture,foreignKey:'resource_id'});
-// picture.belongsToMany(resource,{through:resource_picture,foreignKey:'picture_id'});
-// async function a() {
-//     let user_person = await user.findOne({where:{email:'1558531230@qq.com'}});
-//     let screen_new = await screen.findOne({where:{uuid:'12345678'}});
-//     await user_person.addScreen(screen_new);
-// }
-// a();
-const zip = require('../router/zip');
-let a = new Array();
-a[0]="1.jpg";
-a[1]="2.jpg";
-zip(a,"hahahahahah","q");
+const model = require('./model');
+const config = require('./config');
+const Sequelize = require('sequelize');
+let sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'postgres',
+    timezone:'+08:00',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 3000
+    }
+});
+let user = model.user;
+let screen = model.screen;
+let picture = model.picture;
+let resource = model.resource;
+let resource_picture = model.resource_picture;
+user.hasMany(picture,{foreignKey:'user_id'});
+user.hasMany(screen,{foreignKey:'user_id'});
+user.hasMany(resource,{foreignKey:'user_id'});
+screen.hasMany(resource,{foreignKey:'screen_id'});
+resource.belongsToMany(picture,{through:resource_picture,foreignKey:'resource_id'});
+picture.belongsToMany(resource,{through:resource_picture,foreignKey:'picture_id'});
+async function a() {
+    let resource_new = await resource.findOne({where:{resource_id:1}});
+    await resource_new.destroy();
+}
+a();
+//const fs = require('fs');
+// let data = {};
+// data.i = "qwe";
+// data = JSON.stringify(data);
+// fs.writeFileSync('/home/lcr/1.json',data);
+// let d = fs.readFileSync('/home/lcr/1.json');
+// d = JSON.parse(d);
+// console.log(d.i);
+//fs.unlinkSync('/home/lcr/1.json');
--- router/router.js
+++ router/router.js
@@ -12,18 +12,18 @@ const images = require("image-size");
 const isOnline = require('./isOnlie');
 const fs = require('fs');
 const zip = require('./zip');
-const upDir = '/home/lcr/';
+const upDir = '/home/lcr/file/';
 let user = model.user;
 let screen = model.screen;
 let picture = model.picture;
 let resource = model.resource;
 let resource_picture = model.resource_picture;
-user.hasMany(picture,{foreignKey:'user_id'});
-user.hasMany(screen,{foreignKey:'user_id'});
-user.hasMany(resource,{foreignKey:'user_id'});
-screen.hasMany(resource,{foreignKey:'screen_id'});
-resource.belongsToMany(picture,{through:resource_picture,foreignKey:'resource_id'});
-picture.belongsToMany(resource,{through:resource_picture,foreignKey:'picture_id'});
+user.hasMany(picture, {foreignKey: 'user_id'});
+user.hasMany(screen, {foreignKey: 'user_id'});
+user.hasMany(resource, {foreignKey: 'user_id'});
+screen.hasMany(resource, {foreignKey: 'screen_id'});
+resource.belongsToMany(picture, {through: resource_picture, foreignKey: 'resource_id'});
+picture.belongsToMany(resource, {through: resource_picture, foreignKey: 'picture_id'});
 let sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     dialect: 'postgres',
@@ -40,14 +40,14 @@ let transporter = nodemailer.createTransport({
         pass: 'pobooks126'
     }
 });
-router.post('/action=signup',async (ctx,next)=>{
-    let user_num = user.count({where:{email:ctx.request.body.email}});
+router.post('/action=signup', async (ctx, next) => {
+    let user_num = user.count({where: {email: ctx.request.body.email}});
     let n = Math.floor(Math.random() * 9000 + 1000);
     ctx.session.verify = n.toString();
-    if(user_num===1){
-        ctx.api(200,{},{code:10001,msg:'该用户已存在'});
+    if (user_num === 1) {
+        ctx.api(200, {}, {code: 10001, msg: '该用户已存在'});
     }
-    else{
+    else {
         let mailOptions = {
             from: '"messenger" <pobooks@126.com>',
             to: ctx.request.body.email,
@@ -60,47 +60,47 @@ router.post('/action=signup',async (ctx,next)=>{
             }
             console.log('Message %s sent: %s', info.messageId, info.response);
         });
-        ctx.api(200,{},{code:100000,msg:'验证码已下发'});
+        ctx.api(200, {}, {code: 100000, msg: '验证码已下发'});
     }
     await next();
 });
-router.post('/action=verify',async (ctx,next)=>{
-    if(ctx.session.verify===ctx.request.body.verify){
+router.post('/action=verify', async (ctx, next) => {
+    if (ctx.session.verify === ctx.request.body.verify) {
         await user.create({
-            username:ctx.request.body.username,
-            email:ctx.request.body.email,
-            password:ctx.request.body.password
+            username: ctx.request.body.username,
+            email: ctx.request.body.email,
+            password: ctx.request.body.password
         });
-        ctx.api(200,{},{code:10000,msg:'注册成功！'});
+        ctx.api(200, {}, {code: 10000, msg: '注册成功！'});
     }
-    else ctx.api(200,{},{code:0,msg:'验证码错误！'});
+    else ctx.api(200, {}, {code: 0, msg: '验证码错误！'});
     await next();
 });
-router.post('/action=login',async (ctx,next)=>{
-    let user_num = await user.count({where:{email:ctx.request.body.email}});
-    if(user_num===0) ctx.api(200,{},{code:0,msg:'该用户不存在！'});
-    else{
-        if(ctx.cookies.get('user',{})===undefined){
-            let user_person = await user.findOne({where:{email:ctx.request.body.email}});
-            if(user_person.password===ctx.request.body.password){
+router.post('/action=login', async (ctx, next) => {
+    let user_num = await user.count({where: {email: ctx.request.body.email}});
+    if (user_num === 0) ctx.api(200, {}, {code: 0, msg: '该用户不存在！'});
+    else {
+        if (ctx.cookies.get('user', {}) === undefined) {
+            let user_person = await user.findOne({where: {email: ctx.request.body.email}});
+            if (user_person.password === ctx.request.body.password) {
                 ctx.session.custom_email = user_person.email;
-                let data={};
+                let data = {};
                 data.username = user_person.username;
                 let md = md5(ctx.session.custom_email);
-                if (ctx.request.body.remember_me === true ){
+                if (ctx.request.body.remember_me === true) {
                     ctx.cookies.set(
                         'user',
                         md,
                         {
                             domain: '118.89.197.156',  // 写cookie所在的域名
                             path: '/',       // 写cookie所在的路径
-                            maxAge: 60*60*24*30*1000, // cookie有效时长
+                            maxAge: 60 * 60 * 24 * 30 * 1000, // cookie有效时长
                             httpOnly: true,  // 是否只用于http请求中获取
                             overwrite: true  // 是否允许重写
                         }
                     );
                 }
-                else{
+                else {
                     ctx.cookies.set(
                         'user',
                         md,
@@ -114,114 +114,114 @@ router.post('/action=login',async (ctx,next)=>{
                 }
                 ctx.session.last_login_time = user_person.last_login_time;
                 await user_person.update({
-                    last_login_time:Date.now(),
+                    last_login_time: Date.now(),
                 });
-                ctx.api(200,data,{code:10000,msg:'登录成功！'});
+                ctx.api(200, data, {code: 10000, msg: '登录成功！'});
             }
         }
-        else{
-            let user_person = await user.findOne({where:{email:ctx.session.email}});
+        else {
+            let user_person = await user.findOne({where: {email: ctx.session.email}});
             ctx.session.last_login_time = user_person.last_login_time;
             await user_person.update({
-                last_login_time:Date.now(),
+                last_login_time: Date.now(),
             });
-            let data={};
-            data.username=user_person.username;
-            data.email=user_person.email;
-            ctx.api(200,data,{code:10000,msg:'自动登录成功！'});
+            let data = {};
+            data.username = user_person.username;
+            data.email = user_person.email;
+            ctx.api(200, data, {code: 10000, msg: '自动登录成功！'});
         }
     }
     await next();
 });
-router.post('/action=forget',async (ctx,next)=>{
-    let user_num = await user.count({where:{email:ctx.request.body.email}});
-    if(user_num===0) ctx.api(200,{},{code:0,msg:'不存在该用户！'});
-    else{
-        let user_person = await user.findOne({where:{email:ctx.request.body.email}});
-        if(user_person.username===ctx.request.body.username){
+router.post('/action=forget', async (ctx, next) => {
+    let user_num = await user.count({where: {email: ctx.request.body.email}});
+    if (user_num === 0) ctx.api(200, {}, {code: 0, msg: '不存在该用户！'});
+    else {
+        let user_person = await user.findOne({where: {email: ctx.request.body.email}});
+        if (user_person.username === ctx.request.body.username) {
             ctx.session.forget_email = ctx.request.body.email;
-            ctx.api(200,{},{code:10000,msg:'验证成功！'});
+            ctx.api(200, {}, {code: 10000, msg: '验证成功！'});
         }
-        else{
-            ctx.api(200,{},{code:0,msg:'用户名错误！'});
+        else {
+            ctx.api(200, {}, {code: 0, msg: '用户名错误！'});
         }
     }
     await next();
 });
-router.post('/action=new_password',async (ctx,next)=>{
-    let user_person = await user.findOne({where:{email:ctx.session.forget_email}});
+router.post('/action=new_password', async (ctx, next) => {
+    let user_person = await user.findOne({where: {email: ctx.session.forget_email}});
     await user_person.update({
-        password:ctx.request.body.new_password
+        password: ctx.request.body.new_password
     });
-    ctx.api(200,{},{code:10000,msg:'修改密码成功！'});
+    ctx.api(200, {}, {code: 10000, msg: '修改密码成功！'});
     await next();
 });
-router.post('/action=get_info',async (ctx,next)=>{
-    let user_person = await user.findOne({where:{email:ctx.session.custom_email}});
+router.post('/action=get_info', async (ctx, next) => {
+    let user_person = await user.findOne({where: {email: ctx.session.custom_email}});
     let user_picture = await user_person.getPictures();
     let user_screen = await user_person.getScreens();
-    let data ={};
+    let data = {};
     data.username = user_person.username;
     data.email = user_person.email;
     data.work_place = user_person.work_place;
     data.last_login_time = ctx.session.last_login_time;
     data.picture_num = user_picture.length;
     data.screen_num = user_screen.length;
-    ctx.api(200,data,{code:1,msg:'获取信息成功'});
+    ctx.api(200, data, {code: 1, msg: '获取信息成功'});
     await next();
 });
-router.post('/action=get_screen',async (ctx,next)=>{
-    let user_person = await user.findOne({where:{email:ctx.session.custom_email}});
+router.post('/action=get_screen', async (ctx, next) => {
+    let user_person = await user.findOne({where: {email: ctx.session.custom_email}});
     let user_screen = await user_person.getScreens();
     let data = {};
     data.info = new Array();
-    for(let i =0 ; i < user_screen.length ; ++i) {
+    for (let i = 0; i < user_screen.length; ++i) {
         let resource_name = await user_screen[i].getResources();
-        data.info[i]={
-            uuid:user_screen[i].uuid,
-            status:isOnline(user_screen[i].updated_at,user_screen[i].time),
-            name:user_screen[i].name,
-            update_time:user_screen[i].updated_at,
-            freq:user_screen[i].time,
-            pack:resource_name.name,
-            note:user_screen[i].remark
+        data.info[i] = {
+            uuid: user_screen[i].uuid,
+            status: isOnline(user_screen[i].updated_at, user_screen[i].time),
+            name: user_screen[i].name,
+            update_time: user_screen[i].updated_at,
+            freq: user_screen[i].time,
+            pack: resource_name.name,
+            note: user_screen[i].remark
         }
     }
-    ctx.api(200,data,{code:10000,msg:"获取成功！"});
+    ctx.api(200, data, {code: 10000, msg: "获取成功！"});
     await next();
 });
-router.post('/action=add_screen',async(ctx,next)=>{
-    let user_person = await user.findOne({where:{email:ctx.session.custom_email}});
-    let screen_num = await screen.count({where:{uuid:ctx.request.body.uuid}});
-    if(screen_num===0){
-        ctx.api(200,{},{code:0,msg:'不存在该屏幕！'});
+router.post('/action=add_screen', async (ctx, next) => {
+    let user_person = await user.findOne({where: {email: ctx.session.custom_email}});
+    let screen_num = await screen.count({where: {uuid: ctx.request.body.uuid}});
+    if (screen_num === 0) {
+        ctx.api(200, {}, {code: 0, msg: '不存在该屏幕！'});
     }
-    else{
-        let screen_new = await screen.findOne({where:{uuid:ctx.request.body.uuid}});
+    else {
+        let screen_new = await screen.findOne({where: {uuid: ctx.request.body.uuid}});
         await user_person.addScreen(screen_new);
-        ctx.api(200,{},{code:10000,msg:'添加成功！'});
+        ctx.api(200, {}, {code: 10000, msg: '添加成功！'});
     }
     await next();
 });
-router.post('/action=modify_screen',async (ctx,next)=>{
+router.post('/action=modify_screen', async (ctx, next) => {
     let screen_uuid = ctx.request.body.uuid;
-    for(let i of screen_uuid){
-        let screen_new = await screen.findOne({where:{uuid:i}});
-        if(ctx.request.body.new_name!==undefined) await screen_new.update({name:ctx.request.body.new_name});
-        if(ctx.request.body.new_freq!==undefined) await screen_new.update({time:ctx.request.body.new_freq});
-        if(ctx.request.body.new_note!==undefined) await screen_new.update({remark:ctx.request.body.new_note});
+    for (let i of screen_uuid) {
+        let screen_new = await screen.findOne({where: {uuid: i}});
+        if (ctx.request.body.new_name !== undefined) await screen_new.update({name: ctx.request.body.new_name});
+        if (ctx.request.body.new_freq !== undefined) await screen_new.update({time: ctx.request.body.new_freq});
+        if (ctx.request.body.new_note !== undefined) await screen_new.update({remark: ctx.request.body.new_note});
     }
-    ctx.api(200,{},{code:10000,msg:'修改成功！'});
+    ctx.api(200, {}, {code: 10000, msg: '修改成功！'});
 });
-router.post('/action=del_screen',async (ctx,next)=>{
-    let user_person = await user.findOne({where:{email:ctx.session.custom_email}});
+router.post('/action=del_screen', async (ctx, next) => {
+    let user_person = await user.findOne({where: {email: ctx.session.custom_email}});
     let uuid = ctx.request.body.uuid;
-    for(let i of uuid){
-        let screen_del = await screen.findOne({where:{uuid:i}});
+    for (let i of uuid) {
+        let screen_del = await screen.findOne({where: {uuid: i}});
         await user_person.removeScreen(screen_del);
-        await screen.destroy({where:{uuid:i}});
+        await screen.destroy({where: {uuid: i}});
     }
-    ctx.api(200,{},{code:10000,msg:'删除成功！'});
+    ctx.api(200, {}, {code: 10000, msg: '删除成功！'});
     await next();
 });
 router.post('/action=upload', koaBody({
@@ -229,48 +229,105 @@ router.post('/action=upload', koaBody({
     formidable: {
         uploadDir: upDir
     }
-}),async (ctx, next) => {
+}), async (ctx, next) => {
     let files = ctx.request.body.files;
-    for(let i = 0;i<files.file.length;++i){
+    for (let i = 0; i < files.file.length; ++i) {
         let fileFormat = (files.file[i].name).split(".");
         let file_name = 'picture-' + Date.now() + '.' + fileFormat[fileFormat.length - 1];
-        let user_person = await user.findOne({where: {email:ctx.session.custom_email}});
+        let user_person = await user.findOne({where: {email: ctx.session.custom_email}});
         let image = images(files.file[i].path);
         await user_person.createPicture({
             name: file_name,
             size: files.file[i].size,
-            image_size:image.width.toString()+'×'+ image.height.toString(),
-            image_type:files.file[i].type,
+            image_size: image.width.toString() + '×' + image.height.toString(),
+            image_type: files.file[i].type,
             url: 'http://118.89.197.156:8000/' + file_name
         });
-        fs.rename(files.file[i].path,upDir+file_name,(err)=>{
+        fs.rename(files.file[i].path, upDir + file_name, (err) => {
             console.log(err);
         })
     }
-    ctx.api(200,{},{code:10000, msg: '上传成功'});
+    ctx.api(200, {}, {code: 10000, msg: '上传成功'});
     await next();
 });
-router.post('/action=get_picture',async (ctx,next)=>{
-    let user_person = await user.findOne({where:{email:ctx.session.custom_email}});
+router.post('/action=get_picture', async (ctx, next) => {
+    let user_person = await user.findOne({where: {email: ctx.session.custom_email}});
     let user_person_picture = await user_person.getPictures();
     let data = {};
     data.pictures = new Array();
-    for(let i = 0 ; i < user_person_picture.length ; ++i){
+    for (let i = 0; i < user_person_picture.length; ++i) {
         data.pictures[i] = {};
         data.pictures[i].id = user_person_picture[i].picture_id;
         data.pictures[i].src = user_person_picture[i].url;
     }
-    ctx.api(200,data,{code:10000,msg:'获取图片成功！'});
+    ctx.api(200, data, {code: 10000, msg: '获取图片成功！'});
     await next()
 });
-router.post('/action=',async (ctx,next)=>{
+router.post('/action=create_resource', async (ctx, next) => {
+    let user_person = await user.findOne({where: {email: ctx.session.custom_email}});
+    let picture_id = ctx.request.body.id;
+    let picture_time = ctx.request.body.time;
+    let md5 = md5(Date.now());
+    let main = {},picture_name=new Array();
+    main.md5 = md5;
+    let resource_new = await resource.create({name:ctx.request.body.resource_name});
+    await user_person.addResource(resource_new);
+    for(let i = 0 ; i < picture_id.length ; ++i){
+        let picture_add = await picture.findOne({where:{picture_id:picture_id[i]}});
+        main[picture_add.name] = picture_time[i];
+        picture_name[i] = picture_add.name;
+    }
+    zip(picture_name,JSON.stringify(main),resource_new.name);
+    fs.writeFileSync('/home/ubuntu/file/'+resource_new.resource_id+'.json',main.toString());
+    ctx.api(200,{},{code:10000,msg:'创建资源包成功！'});
     await next();
 });
-router.post('/action=modify_user',async (ctx,next)=>{
+router.post('/action=get_resource',async (ctx,next)=>{
     let user_person = await user.findOne({where:{email:ctx.session.custom_email}});
-    if(ctx.request.body.new_username!==undefined) await user_person.update({username:ctx.request.body.new_username});
-    if(ctx.request.body.new_work_place!==undefined) await user_person.update({work_place:ctx.request.body.new_work_place});
-    ctx.api(200,{},{code:10000,msg:'修改信息成功！'});
+    let person_resource = await user_person.getResources();
+    let data = {};
+    data.resources = new Array();
+    for(let i =0;i<person_resource.length;++i){
+        let get_screen = await person_resource[i].getScreens();
+        data.resources[i].screen = get_screen;
+        data.resources[i].name = person_resource[i].name;
+        data.resources[i].note = person_resource[i].remark;
+        data.resources[i].resource_id = person_resource[i].resource_id;
+    }
+    ctx.api(200,data,{code:10000,msg:'获取资源包成功！'});
+    await next();
+});
+router.post('/action=get_resource_info',async (ctx,next)=>{
+    let user_person = await user.findOne({where: {email: ctx.session.custom_email}});
+    let user_person_picture = await user_person.getPictures();
+    let resource_get = await resource.findOne({where:{resource_id:ctx.request.body.resource_id}});
+    let resource_settings = JSON.parse(fs.readFileSync(upDir+resource_get.resource_id+'.json'));
+    let data={};
+    data.used_pictures = resource_settings;
+    for (let i = 0; i < user_person_picture.length; ++i) {
+        data.pictures[i] = {};
+        data.pictures[i].id = user_person_picture[i].picture_id;
+        data.pictures[i].src = user_person_picture[i].url;
+    }
+    ctx.api(200, data, {code: 10000, msg: '获取资源包图片成功！'});
+    await next();
+});
+router.post('/action=set_resource',async (ctx,next)=>{});
+router.post('/action=del_resource',async (ctx,next)=>{
+    let del_resource = await resource.findOne({where:{resource_id:ctx.request.body.resource_id}});
+    fs.unlinkSync(upDir+del_resource.name+'.zip');
+    fs.unlinkSync(upDir+del_resource.resource_id+'.zip');
+    let del_resource_picture = await del_resource.getPictures();
+    await del_resource.removePictures(del_resource_picture);
+    await del_resource.destroy();
+    ctx.api(200,{},{code:10000,msg:'删除成功！'});
+    await next();
+});
+router.post('/action=modify_user', async (ctx, next) => {
+    let user_person = await user.findOne({where: {email: ctx.session.custom_email}});
+    if (ctx.request.body.new_username !== undefined) await user_person.update({username: ctx.request.body.new_username});
+    if (ctx.request.body.new_work_place !== undefined) await user_person.update({work_place: ctx.request.body.new_work_place});
+    ctx.api(200, {}, {code: 10000, msg: '修改信息成功！'});
     await next();
 });
 module.exports = router;
