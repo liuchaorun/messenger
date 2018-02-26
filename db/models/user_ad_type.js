fcce7f37724f51956298669541136db95c991ab1
Soulike <316413310@qq.com>
2018-02-26 14:42:49 +0800

    修复bug

--- db/models/user_ad_type.js
+++ db/models/user_ad_type.js
@@ -2,5 +2,5 @@ const database = require("../database");
 const Sequelize = require('sequelize');
 module.exports=database.defineModel('user_ad_type',{
     user_id:Sequelize.BIGINT,
-    ad_type_id::Sequelize.BIGINT
+    ad_type_id:Sequelize.BIGINT
 });
