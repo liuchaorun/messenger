9ec256cb8752345cd83c190624bce97dec880122
lcr <1558531230@qq.com>
2018-02-27 15:05:01 +0800

    修复数据库bug1.7

--- router/router.js
+++ router/router.js
@@ -245,7 +245,7 @@ router.post('/action=upload', koaBody({
     }
 }), async (ctx, next) => {
     let files = ctx.request.body.files;
-    console.log('body'+ctx.request.body);
+    console.log(ctx.body.name);
     console.log(ctx.request.body.files.name);
     if(files.file.name === undefined){
         for (let i = 0; i < files.file.length; ++i) {
